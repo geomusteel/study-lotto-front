09d0a259c996ea045c52d75ccb6c05644886cae8
geomusteel <geomusteel@naver.com>
2024-03-24 23:02:26 +0900

    first commit

---
+++ src/common/typography/typography.ts
@@ -0,0 +1,13 @@
+const fontSize = {
+    100: "2.4rem",
+    200: "2rem",
+    300: "1.8rem",
+    400: "1.6rem",
+    500: "1.4rem",
+    600: "1.2rem",
+    700: "1rem",
+};
+
+export const font = {
+    fontSize,
+};
